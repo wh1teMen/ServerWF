197a13c2ae301ce5b03eaa1997cf2ec6283f1e07
wh1teMen <aleexandr777@gmail.com>
2023-07-18 21:41:20 +0500

    Добавьте файлы проекта.

---
+++ ServerWF/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ServerWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox_out = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox_out
+            // 
+            this.textBox_out.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_out.Location = new System.Drawing.Point(12, 21);
+            this.textBox_out.Name = "textBox_out";
+            this.textBox_out.Size = new System.Drawing.Size(659, 49);
+            this.textBox_out.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(659, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ON";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 150;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 121);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_out);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сервер";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_out;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ServerWF/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ServerWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            size();
+        }
+        Socket s = null;
+        private  void button1_Click(object sender, EventArgs e)
+        {           
+            Task.Run(() => { Lisener(); });                  
+        }
+       string text = string.Empty;
+        string textTmp = string.Empty;
+       
+        public void Lisener()
+        {
+            Text = "Сервер запущен";
+            try
+            {
+                s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                IPAddress ip = IPAddress.Parse("127.0.0.1");
+                IPEndPoint ep = new IPEndPoint(ip, 7777);
+                s.Bind(ep);
+                s.Listen(10);
+                while (true)
+                {
+                    textBox_out.Clear();
+                    byte[] buffer_input = new byte[1024];
+                    Socket ns = s.Accept();
+                    var l = ns.Receive(buffer_input);
+                    text += "" + text;
+                    text += Encoding.UTF8.GetString(buffer_input, 0, l);
+                    textTmp = text;
+                    ns.Shutdown(SocketShutdown.Both);
+                    ns.Close();
+                }
+            }
+            catch { }          
+        }
+       
+         
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (textTmp.Length > 0) {
+                textTmp = textTmp.Substring(1) + textTmp[0];
+                textBox_out.Text = textTmp;               
+                
+            }           
+        }
+        private void size()
+        {
+           this.Width = 710;
+           this.Height = 160;      
+           this.MinimumSize = this.MaximumSize = this.Size;
+        }
+        
+
+    }
+
+}
+
+
